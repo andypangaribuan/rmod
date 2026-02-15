c9c9ebc1583334d4fd932bb9a8feef62795db061
Andy Pangaribuan <iam.pangaribuan@gmail.com>
2026-02-15 23:04:02 +0700

    update

--- src/util/support.rs
+++ src/util/support.rs
@@ -9,10 +9,20 @@
  */
 
 pub use chrono;
+use std::collections::HashSet;
 
 #[macro_export]
 macro_rules! log {
     ($($arg:tt)*) => {
         println!("{} {}", chrono::Local::now().format("%Y-%m-%d %H:%M:%S%.3f %:z"), format!($($arg)*));
     };
+}
+
+pub fn collect_unique<T, K, F>(items: Vec<T>, f: F) -> Vec<K>
+where
+    K: std::hash::Hash + Eq + Clone,
+    F: Fn(T) -> K,
+{
+    let mut seen = HashSet::new();
+    items.into_iter().map(f).filter(|k| seen.insert(k.clone())).collect()
 }
