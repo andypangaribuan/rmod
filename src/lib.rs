bd080acec15ccf4d3bf04922643bf8abcffad6a9
Andy Pangaribuan <iam.pangaribuan@gmail.com>
2026-02-25 16:31:16 +0700

    update

--- src/lib.rs
+++ src/lib.rs
@@ -30,8 +30,7 @@ pub use types::ArcX;
 // Proxy re-exports for common dependencies
 pub use ::serde;
 pub use ::serde::{Deserialize, Serialize, de, ser};
-pub use ::serde_json;
-pub use ::serde_json::*;
+pub use ::serde_json as json;
 pub use chrono;
 pub use chrono::{DateTime, Datelike, FixedOffset, Local, NaiveDate, NaiveDateTime, NaiveTime, TimeZone, Timelike, Utc};
 pub use rust_decimal;
